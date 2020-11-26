18153a7fceb46d4741af191f6fad38558f1801b0
Asier1812 <asierjimen1812@gmail.com>
2020-11-26 17:25:10 +0100

    First commit

---
+++ Clase.py
@@ -0,0 +1,69 @@
+import discord
+import datetime
+from asyncio import sleep
+
+
+class MyClient(discord.Client):
+
+    def __init__(self):
+        self.stateid = 0
+        discord.Client.__init__(self)
+    
+    async def on_ready(self):
+        print('Logged on as {0}!'.format(self.user))
+        print(self.guilds)
+    async def on_message(self, message):
+        if (message.content.startswith("-")):
+            if (message.author.voice is None or message.author.voice.channel is None):
+                print("author none")
+                return
+            if (message.content=="-clase"):
+                voice_channel = message.author.voice.channel
+                vc = message.author.guild.voice_client
+                if (not vc):    
+                    vc = await voice_channel.connect()
+                dia = datetime.datetime.now().weekday() + 1
+                file = "nohay.mp3"
+                if (dia < 4 or dia == 7):
+                    file = "hay.mp3"
+                if(vc.is_playing()):
+                    vc.stop()
+                self.stateid += 1
+                ownid = self.stateid
+                vc.play(discord.FFmpegPCMAudio(file), after=lambda e: print('done', e))
+                await sleep(11.35)
+                if (ownid == self.stateid):
+                    await vc.disconnect()
+            
+            elif (message.content.startswith("-clase")):
+                voice_channel = message.author.voice.channel
+                vc = message.author.guild.voice_client
+                if (not vc):    
+                    vc = await voice_channel.connect()
+                
+                numero = message.content.split()[1]
+                if (numero.lower() in "lunes martes miercoles miércoles jueves viernes sabado sábado domingo".split()):
+                    dia = ("lunes martes miercoles miércoles jueves viernes sabado sábado domingo".split()).index(numero.lower())
+                    if (dia >= 3):
+                        dia -= 1
+                    if (dia >= 7):
+                        dia -= 1
+                        
+                else:
+                    dia = (datetime.datetime.now().weekday() + int(numero)) % 7
+                file = "nohay.mp3"
+                if (dia < 4 or dia >= 7):
+                    file = "hay.mp3"
+
+                if(vc.is_playing()):
+                    vc.stop()
+                self.stateid += 1
+                ownid = self.stateid
+                vc.play(discord.FFmpegPCMAudio(file), after=lambda e: print('done', e))
+                await sleep(11.5)
+                if (ownid == self.stateid):
+                    await vc.disconnect()
+
+
+client = MyClient()
+client.run("Nzc0Mzc4MDkzMzY4MTgwNzk3.X6W5zA.Z44fzZg0c_dbOeurUuqlxq3TkKc")
