e2f9d0d566cdb7c2c52594b713df79026d036ef1
Asier1812 <asierjimen1812@gmail.com>
2020-11-26 22:20:44 +0100

    xinxonxin

--- Trollendo.py
+++ Trollendo.py
@@ -14,10 +14,10 @@ class MyClient(discord.Client):
     async def on_message(self, message):
         if (message.content.startswith("-")):
             text_channel = message.channel
-            if (message.author.voice is None or message.author.voice.channel is None):
-                await text_channel.send(message.author.mention + ", como vas a saber si hay clase sin meterte a un canal, melón.")
-                return
             if (message.content=="-clase"):
+                if (message.author.voice is None or message.author.voice.channel is None):
+                    await text_channel.send(message.author.mention + ", como vas a saber si hay clase sin meterte a un canal, melón.")
+                    return
                 voice_channel = message.author.voice.channel
                 vc = message.author.guild.voice_client
                 if (not vc):
@@ -36,6 +36,9 @@ class MyClient(discord.Client):
                     await vc.disconnect()
             
             elif (message.content.startswith("-clase")):
+                if (message.author.voice is None or message.author.voice.channel is None):
+                    await text_channel.send(message.author.mention + ", como vas a saber si hay clase sin meterte a un canal, melón.")
+                    return
                 voice_channel = message.author.voice.channel
                 vc = message.author.guild.voice_client
                 if (not vc):    
@@ -63,6 +66,26 @@ class MyClient(discord.Client):
                 await sleep(11.5)
                 if (ownid == self.stateid):
                     await vc.disconnect()
+                    
+            elif (message.content.startswith("-celebrate")):
+                if (message.author.voice is None or message.author.voice.channel is None):
+                    return
+                voice_channel = message.author.voice.channel
+                vc = message.author.guild.voice_client
+                if (not vc):    
+                    vc = await voice_channel.connect()
+                           
+                file = "celebrate.aac"
+
+                if(vc.is_playing()):
+                    vc.stop()
+                self.stateid += 1
+                ownid = self.stateid
+                vc.play(discord.FFmpegPCMAudio(file))
+                await sleep(13)
+                if (ownid == self.stateid):
+                    await vc.disconnect()
+
 
 
 client = MyClient()
